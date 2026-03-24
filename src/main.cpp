13a487aaae1b75e741856228c3bfcdaeb7bc894a
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-03-24 10:23:09 +0700

    feat: plane 3d

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,37 @@
+#include "stima/voxelizer.hpp"
 #include <iostream>
+using namespace std;
 
-int main() {
-    
+int main()
+{
+    cout << "Masukkan input path: ";
+    string path;
+    getline(cin, path);
+    cout << "Masukkan kedalaman maksimal: ";
+    int maxDepth;
+    cin >> maxDepth;
+
+    stima::Voxelizer voxelizer = stima::Voxelizer();
+    try {
+        voxelizer.voxelize(path, maxDepth);
+        cout << "Voxel Count: " << voxelizer.getVoxelCount() << endl;
+        cout << "Vertice Count: " << voxelizer.getResultVertices().size() << endl;
+        cout << "Face Count: " << voxelizer.getResultFaces().size() << endl;
+        cout << "Traversed node: " << endl;
+        auto &traversedCount = voxelizer.getTraversedCount();
+        for (size_t i = 0; i < traversedCount.size(); i++) {
+            cout << "Depth " << i << ": " << traversedCount[i] << endl;
+        }
+        cout << "Not traversed node: " << endl;
+        auto &notTraversedCount = voxelizer.getNotTraversedCount();
+        for (size_t i = 0; i < notTraversedCount.size(); i++) {
+            cout << "Depth " << i << ": " << notTraversedCount[i] << endl;
+        }
+    }
+    catch (const exception &e) {
+        cout << "Gagal melakukan voxelisasi..." << endl;
+        cout << e.what() << endl;
+    }
+
+    return 0;
 }
---
+++ src/stima/geometry/Plane3D.hpp
@@ -0,0 +1,23 @@
+#ifndef STIMA_PLANE_3D_H
+#define STIMA_PLANE_3D_H
+#include "Point3D.hpp"
+#include "Line3D.hpp"
+
+namespace stima {
+    class Plane3D {
+    public:
+        Plane3D(Point3D p1, Point3D p2, Point3D p3);
+        Point3D getNormal();
+        bool isParallel(Line3D line);
+        bool isParallel(Plane3D plane);
+        bool isIntersect(Line3D line);
+        Point3D getIntersection(Line3D line);
+        std::string toString();
+
+    private:
+        // Plane ax + by + cz + d = 0
+        double a, b, c, d;
+    };
+}
+
+#endif
