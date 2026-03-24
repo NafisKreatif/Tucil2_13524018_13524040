427c429533068d6b605967d7a69ced6f4afa2dd1
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-03-24 10:23:22 +0700

    feat: add more operator in Point3D

--- src/stima/geometry/Point3D.cpp
+++ src/stima/geometry/Point3D.cpp
@@ -1,8 +1,10 @@
 #include "Point3D.hpp"
 #include <math.h>
+#include <string>
 
 namespace stima {
     // Constructor
+    Point3D::Point3D() : Point3D(0, 0, 0) {};
     Point3D::Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
 
     // Operators
@@ -28,6 +30,17 @@ namespace stima {
         this->z -= rhs.z;
         return *this;
     }
+    Point3D operator*(const Point3D &lhs, const Point3D &rhs)
+    {
+        return Point3D(lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z);
+    }
+    Point3D Point3D::operator*=(const Point3D &rhs)
+    {
+        this->x *= rhs.x;
+        this->y *= rhs.y;
+        this->z *= rhs.z;
+        return *this;
+    }
     Point3D operator*(double lhs, const Point3D &rhs)
     {
         return Point3D(lhs * rhs.x, lhs * rhs.y, lhs * rhs.z);
@@ -47,12 +60,64 @@ namespace stima {
     {
         return Point3D(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs);
     }
+    Point3D operator-(const Point3D &rhs)
+    {
+        return rhs * -1;
+    }
     Point3D Point3D::operator/=(double rhs)
     {
         this->x /= rhs;
         this->y /= rhs;
         this->z /= rhs;
         return *this;
+    }
+    bool Point3D::operator<(const Point3D &rhs)
+    {
+        if (this->x < rhs.x) return true;
+        if (this->x > rhs.x) return false;
+        else {
+            if (this->y < rhs.y) return true;
+            if (this->y > rhs.y) return false;
+            else {
+                return this->z < rhs.z;
+            }
+        }
+    }
+    bool Point3D::operator<=(const Point3D &rhs)
+    {
+        if (this->x < rhs.x) return true;
+        if (this->x > rhs.x) return false;
+        else {
+            if (this->y < rhs.y) return true;
+            if (this->y > rhs.y) return false;
+            else {
+                return this->z <= rhs.z;
+            }
+        }
+    }
+    bool Point3D::operator>(const Point3D &rhs)
+    {
+        if (this->x < rhs.x) return false;
+        if (this->x > rhs.x) return true;
+        else {
+            if (this->y < rhs.y) return false;
+            if (this->y > rhs.y) return true;
+            else {
+                return this->z > rhs.z;
+            }
+        }
+    }
+    bool Point3D::operator>=(const Point3D &rhs)
+    {
+        if (this->x < rhs.x) return false;
+        if (this->x > rhs.x) return true;
+        else {
+            if (this->y < rhs.y) return false;
+            if (this->y > rhs.y) return true;
+            else {
+                return this->z >= rhs.z;
+            }
+        }
     }
 
     // Methods
@@ -71,5 +136,14 @@ namespace stima {
     Point3D Point3D::crossProduct(const Point3D &p)
     {
         return Point3D(y * p.z - z * p.y, z * p.x - x * p.z, x * p.y - y * p.x);
+    }
+    bool Point3D::isApproximately(const Point3D &p, double epsilon)
+    {
+        return abs(x - p.x) < epsilon && abs(y - p.y) < epsilon && abs(z - p.z) < epsilon;
+    }
+
+    std::string Point3D::toString()
+    {
+        return "(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + ")";
     }
 }
--- src/stima/geometry/Point3D.hpp
+++ src/stima/geometry/Point3D.hpp
@@ -1,5 +1,6 @@
 #ifndef STIMA_POINT_3D_H
 #define STIMA_POINT_3D_H
+#include <string>
 
 namespace stima {
     class Point3D {
@@ -8,24 +9,37 @@ namespace stima {
         double x, y, z;
 
         // Constructor
+        Point3D();
         Point3D(double x, double y, double z);
 
-        // Operators
+        // Mathematical Operators
         friend Point3D operator+(const Point3D &lhs, const Point3D &rhs);
         Point3D operator+=(const Point3D &rhs);
         friend Point3D operator-(const Point3D &lhs, const Point3D &rhs);
         Point3D operator-=(const Point3D &rhs);
+        friend Point3D operator*(const Point3D &lhs, const Point3D &rhs);
+        Point3D operator*=(const Point3D &rhs);
         friend Point3D operator*(double lhs, const Point3D &rhs);
         friend Point3D operator*(const Point3D &lhs, double rhs);
         Point3D operator*=(double rhs);
         friend Point3D operator/(const Point3D &lhs, double rhs);
         Point3D operator/=(double rhs);
+        friend Point3D operator-(const Point3D &rhs);
+
+        // Comparison Operators (for std set)
+        bool operator<(const Point3D &rhs);
+        bool operator<=(const Point3D &rhs);
+        bool operator>(const Point3D &rhs);
+        bool operator>=(const Point3D &rhs);
 
         // Methods
         double magnitude();
         Point3D normalize();
         double dotProduct(const Point3D &p);
         Point3D crossProduct(const Point3D &p);
+        bool isApproximately(const Point3D &p, double epsilon = 1e-6);
+
+        std::string toString();
     };
 }
 
